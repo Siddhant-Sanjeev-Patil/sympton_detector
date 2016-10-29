e7211ed40a5ea3a4966dd88d3e5c39764853d112
Nilesh Prasad <prasadnilesh96@gmail.com>
2016-10-29 23:40:15 +0530

    dynamically adding dropdown list in second step

--- index.php
+++ index.php
@@ -78,10 +78,10 @@
 
         <div class="form-group col-sm-6 col-xs-6 col-lg-6 col-md-6" id="gender" >
             <label class="radio-inline">
-                  <input type="radio" name="male" checked="checked">Male
+                  <input type="radio" class="form-check-input" name="gender" id="male"  checked>Male
             </label>
             <label class="radio-inline">
-                  <input type="radio" name="female">Female
+                  <input type="radio" class="form-check-input" name="gender" id="female"  >Female
             </label>
         </div>
 
@@ -145,7 +145,39 @@
             {
 
                 
-                      echo " First step completed..";
+                      //echo " First step completed..";
+
+                $gender=$_POST['gender'];
+                $birth_year=$_POST['year_of_birth'];
+                $body_location=$_POST['body_location_id'];   
+                echo '<div class="container" id="body_sublocations">
+                <h4>Select sub-location where you are exactly feeling the problem ..</h4>
+            <select class="form-control" name="body_sublocation_id">   ';
+
+              /*
+                $url="https://sandbox-healthservice.priaid.ch/body/locations//16?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InByYXNhZG5pbGVzaDk2QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNzA2IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE2LTA5LTE2IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE0Nzc3NjA5MTAsIm5iZiI6MTQ3Nzc1MzcxMH0.iiMGYvCdUDutCz2U0OjMPW05ZO7b78tWr__bz4Ow3VI&language=en-gb&format=json";*/
+
+
+                $url="https://sandbox-healthservice.priaid.ch/body/locations//".$body_location."?token=".$token."&language=en-gb&format=json";
+              
+
+              $loc_cont = file_get_contents($url);
+              $loc_obj = json_decode($loc_cont,true);   
+              //echo $loc_obj;  
+              //echo json_encode($loc_obj); 
+              $count=0;
+              while(isset($loc_obj[$count]))
+              {
+                //echo $loc_obj[$count]['Name'];
+                //echo $loc_obj[$count]['ID'];
+                //echo '<br><br>';
+                echo '<option value=' .$loc_obj[$count]['ID']. ">" .$loc_obj[$count]['Name']. "</a></option>";              
+
+                $count++;
+              }
+
+
+
 
             }
 
