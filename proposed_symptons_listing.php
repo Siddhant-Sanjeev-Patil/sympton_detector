34e7a2e256e585ec22bb1bbe988d1ff871741e09
Nilesh Prasad <prasadnilesh96@gmail.com>
2016-10-31 16:14:45 +0530

    final results getting displayed, ui to be imroved

--- proposed_symptons_listing.php
+++ proposed_symptons_listing.php
@@ -3,6 +3,18 @@ session_start();
 //echo "i am successful";//for debugging....
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SymptomDetector</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <body>
+
 <?php
 if(isset($_POST['step4_complete']))
               {
@@ -187,8 +199,8 @@ if(isset($_POST['step4_complete']))
                                 $diagnosis_url.="&language=en-gb&format=json";
                                 $specialisations_url.="&language=en-gb&format=json";
 
-                                var_dump($diagnosis_url);//works awesome
-                                var_dump($specialisations_url);//works awesome
+                                //var_dump($diagnosis_url);//works awesome
+                                //var_dump($specialisations_url);//works awesome
 
                                 $diagnosis_loc_cont = file_get_contents($diagnosis_url);
                                 $diagnosis_loc_obj = json_decode($diagnosis_loc_cont,true);  
@@ -196,17 +208,28 @@ if(isset($_POST['step4_complete']))
                                 $specialisation_loc_cont = file_get_contents($specialisations_url);
                                 $specialisation_loc_obj = json_decode($specialisation_loc_cont,true);      
                             
-                                echo json_encode($diagnosis_loc_obj);   
-                                echo "<br><br><br>";
+                                //echo json_encode($diagnosis_loc_obj);   
+                                //echo "<br><br><br>";
 
-                                echo json_encode($specialisation_loc_obj); 
-                                /*
+                                //echo json_encode($specialisation_loc_obj); 
+                                echo '<div class="container">
+                                			<h2>Possible Diseases/Problems</h2></div>';
+                                $x=0;
 
-                                /*https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[%22179%22,%22203%22]&gender=male&year_of_birth=1988&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InByYXNhZG5pbGVzaDk2QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNzA2IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE2LTA5LTE2IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE0Nzc5MTE0NTAsIm5iZiI6MTQ3NzkwNDI1MH0.Rk1RbTxRTwrTg-3JZHpGCASGSdQeaZBg3ReghalSc4M&language=en-gb&format=json
-            --
+                                while(isset($diagnosis_loc_obj[$x]['Issue']['Name']))
+                                {
 
-            https://sandbox-healthservice.priaid.ch/diagnosis/?symptoms=[%22179%22,%22203%22]&amp;gender=male&amp;year_of_birth=1990&amp;token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InByYXNhZG5pbGVzaDk2QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNzA2IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMjAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6Ijk5OTk5OTk5OSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IlByZW1pdW0iLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDE2LTA5LTE2IiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LWF1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE0Nzc5MDQzOTIsIm5iZiI6MTQ3Nzg5NzE5Mn0.MQMB5FJqd-JSKMSAySP1t16j7yJ_NOXbt8ZnhtKxlfw
-            */
+                                echo '
+                                	<div class="container-fluid>"
+                                		<div class="jumbotron" >
+                                  				<b>'.$diagnosis_loc_obj[$x]['Issue']["Name"].'</b><br>'.$diagnosis_loc_obj[$x]['Issue']["Accuracy"].'</div></div>'
+                                		
+                                			;
+                                			$x++;
+
+                                }
+
+                                
 
                                           
 
